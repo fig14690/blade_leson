52709b06163b6aae48ffccd4eff84ca797b11dff
unknown <C:\Users\FIG\AppData\Roaming\The Bat!>
2019-09-07 17:15:41 +0300

    Blog Sidebar in layout

--- resources/cache/a5be33104dac71b53d4667cbf221685c58ac5cc2.php
+++ resources/cache/a5be33104dac71b53d4667cbf221685c58ac5cc2.php
@@ -193,11 +193,166 @@
         </div>
     </div>
     <!-- ##### Breadcrumb Area End ##### -->
-<?php echo $__env->yieldContent('body'); ?>
+
+ <!-- ##### Blog Content Area Start ##### -->
+ <section class="blog-content-area section-padding-0-100">
+     <div class="container">
+        <div class="row justify-content-center">
+            <!-- Blog Posts Area -->
+                      
+                <?php echo $__env->yieldContent('body'); ?>
+
+             <!-- Blog Sidebar Area -->
+             <div class="col-12 col-sm-9 col-md-6 col-lg-4">
+                    <div class="post-sidebar-area">
+
+                        
+                        <!-- ##### Single Widget Area ##### -->
+                        <div class="single-widget-area">
+                            <!-- Title -->
+                            <div class="widget-title">
+                                <h6>Categories</h6>
+                            </div>
+                            <ol class="foode-catagories">
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> Drink</span> <span>(18)</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> Foody</span> <span>(28)</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> Tea</span> <span>(15)</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> Coffee</span> <span>(27)</span></a></li>
+                            </ol>
+                        </div>
+
+                        <!-- ##### Single Widget Area ##### -->
+                        <div class="single-widget-area">
+                            <!-- Title -->
+                            <div class="widget-title">
+                                <h6>Latest Posts</h6>
+                            </div>
+
+                            <!-- Single Latest Posts -->
+                            <div class="single-latest-post d-flex">
+                                <div class="post-thumb">
+                                    <img src="img/blog-img/lp1.jpg" alt="">
+                                </div>
+                                <div class="post-content">
+                                    <a href="#" class="post-title">
+                                        <h6>Weeknight Ponzu Pasta</h6>
+                                    </a>
+                                    <a href="#" class="post-author"><span>by</span> Erin Alderson</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Latest Posts -->
+                            <div class="single-latest-post d-flex">
+                                <div class="post-thumb">
+                                    <img src="img/blog-img/lp2.jpg" alt="">
+                                </div>
+                                <div class="post-content">
+                                    <a href="#" class="post-title">
+                                        <h6>The Most Popular Recipe Last Month</h6>
+                                    </a>
+                                    <a href="#" class="post-author"><span>by</span> Erin Alderson</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Latest Posts -->
+                            <div class="single-latest-post d-flex">
+                                <div class="post-thumb">
+                                    <img src="img/blog-img/lp3.jpg" alt="">
+                                </div>
+                                <div class="post-content">
+                                    <a href="#" class="post-title">
+                                        <h6>A Really Good Chana Masala</h6>
+                                    </a>
+                                    <a href="#" class="post-author"><span>by</span> Erin Alderson</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Latest Posts -->
+                            <div class="single-latest-post d-flex">
+                                <div class="post-thumb">
+                                    <img src="img/blog-img/lp4.jpg" alt="">
+                                </div>
+                                <div class="post-content">
+                                    <a href="#" class="post-title">
+                                        <h6>Spicy Instant Pot Taco Soup</h6>
+                                    </a>
+                                    <a href="#" class="post-author"><span>by</span> Erin Alderson</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Latest Posts -->
+                            <div class="single-latest-post d-flex">
+                                <div class="post-thumb">
+                                    <img src="img/blog-img/lp5.jpg" alt="">
+                                </div>
+                                <div class="post-content">
+                                    <a href="#" class="post-title">
+                                        <h6>Lime Leaf Miso Soup</h6>
+                                    </a>
+                                    <a href="#" class="post-author"><span>by</span> Erin Alderson</a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <!-- ##### Single Widget Area ##### -->
+                        <div class="single-widget-area">
+                            <!-- Adds -->
+                            <a href="#"><img src="img/blog-img/add.png" alt=""></a>
+                        </div>
+
+                        <!-- ##### Single Widget Area ##### -->
+                        <div class="single-widget-area">
+                            <!-- Title -->
+                            <div class="widget-title">
+                                <h6>Archives</h6>
+                            </div>
+                            <ol class="foode-archives">
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> January 2018</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> February 2018</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> March 2018</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> April 2018</span></a></li>
+                                <li><a href="#"><span><i class="fa fa-stop" aria-hidden="true"></i> May 2018</span></a></li>
+                            </ol>
+                        </div>
+
+                        <!-- ##### Single Widget Area ##### -->
+                        <div class="single-widget-area">
+                            <!-- Title -->
+                            <div class="widget-title">
+                                <h6>popular tags</h6>
+                            </div>
+                            <!-- Tags -->
+                            <ol class="popular-tags d-flex flex-wrap">
+                                <li><a href="#">Creative</a></li>
+                                <li><a href="#">Unique</a></li>
+                                <li><a href="#">Template</a></li>
+                                <li><a href="#">Photography</a></li>
+                                <li><a href="#">travel</a></li>
+                                <li><a href="#">lifestyle</a></li>
+                                <li><a href="#">Wordpress Template</a></li>
+                                <li><a href="#">food</a></li>
+                                <li><a href="#">Idea</a></li>
+                            </ol>
+                        </div>
+
+                </div>
+            </div>
+        </div>
+     </div>
+</section>                 
 
 
- <!-- ##### Instagram Area Start ##### -->
- <div class="follow-us-instagram">
+
+
+
+
+    
+
+   
+
+<!-- ##### Instagram Area Start ##### -->
+<div class="follow-us-instagram">
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -278,15 +433,17 @@
             <div class="row">
                 <div class="col-12">
                     <div class="copywrite-text">
-                        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
                 </div>
             </div>
         </div>
- </footer>
+    </footer>
     <!-- ##### Footer Area Start ##### -->
+
 
      <!-- ##### All Javascript Script ##### -->
     <!-- jQuery-2.2.4 js -->
